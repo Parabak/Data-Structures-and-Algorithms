59e15863e2a7635a0a57c225928bc126392b5a2c
Aliaksandr Baranouski <al.baranovski89@gmailcom>
2020-04-03 11:08:25 +0200

    Initial commit

---
+++ SortingAlgorithms/RandomCollection.swift
@@ -0,0 +1,33 @@
+//
+//  RandomCollection.swift
+//  SortingAlgorithms
+//
+//  Created by Aliaksandr Baranouski on 27/03/2020.
+//  Copyright © 2020 personal. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element == Int {
+    
+    mutating func fillWithRandomElements(n: Int, range: ClosedRange<Int>) {
+        
+        for i in 0..<n {
+            self[i] = Int.random(in: range)
+        }
+     }
+   
+    
+    func sum() -> Int {
+
+        switch self.count {
+        case 0:
+            return 0
+        case 1:
+            return self.first!
+        default:
+
+            return (self.first ?? 0) + Array(self.dropFirst()).sum()
+        }
+    }
+}
